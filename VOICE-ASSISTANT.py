2b7c82fe0a9fc8ad0567a23e53533467fcdfdc86
LAVLESH2343 <lavleshsoni51@gmail.com>
2024-09-21 22:37:10 +0530

    update

---
+++ VOICE-ASSISTANT.py
@@ -0,0 +1,10 @@
+import pyttsx3 as p 
+import speech_recognition as sr
+
+engine = p.init("sapi5")
+voice = engine.getProperty('voices')
+engine.setProperty('voice',voice[0].id)
+engine.setProperty('rate',200)
+engine.setProperty('volume',1.0)
+engine.say("hello")
+engine.runAndWait()
